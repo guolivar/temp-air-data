c0c113e7cd5db094e55cb972245960d7ce866543
Gustavo Olivares <guolivar@gmail.com>
2017-02-08 01:19:42 +1300

    I have a plan for the Auckland data obtained from LAWA
    Signed-off-by: Gustavo Olivares <guolivar@gmail.com>

--- get_aq_scratch.py
+++ get_aq_scratch.py
@@ -1,6 +1,21 @@
 import requests
-r = requests.get('https://www.lawa.org.nz/umbraco/api/airservice/getLatestSample?baseUrlPageId=34260&featureOfInterest=67&indicator=pm10&timeIntervalText=Hours&timescaleText=Hourly')
-parsed=r.json()
-print("PM10",parsed["NumericValue"])
-print("Date",parsed["DateTime"])
-print("ERROR",parsed["HasError"])
+
+'''
+    Sites ID
+    Glen Eden:  67 (pm10, no2)
+    Queen St:   9 (no2)
+    Henderson: 5 (pm10, no2)
+    Pakuranga:  1 (pm10)
+    Penrose:    7 (pm10, no2, so2)
+    Patumahoe:   2 (pm10, no2)
+    Takapuna:   23 (pm10, no2)
+'''
+
+pm10 = requests.get('https://www.lawa.org.nz/umbraco/api/airservice/getLatestSample?baseUrlPageId=34260&featureOfInterest=67&indicator=pm10&timeIntervalText=Hours&timescaleText=Hourly').json()
+no2 = requests.get('https://www.lawa.org.nz/umbraco/api/airservice/getLatestSample?baseUrlPageId=34260&featureOfInterest=9&indicator=no2&timeIntervalText=Hours&timescaleText=Hourly').json()
+
+print("Date",pm10["Date"])
+print("Time",pm10["Time"])
+print("PM10 - Glen Eden",pm10["NumericValue"])
+print("NO2 - Queen Street",no2["NumericValue"])
+print("ERROR",pm10["HasError"])
