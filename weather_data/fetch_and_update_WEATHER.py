22f548b57001068898d2384030727d1dab8138b7
Gustavo Olivarescd <guolivar@gmail.com>
2017-02-16 14:31:52 +1300

    re-scaled AQ and initiated weather
    Signed-off-by: Gustavo Olivarescd <guolivar@gmail.com>

---
+++ weather_data/fetch_and_update_WEATHER.py
@@ -0,0 +1,5 @@
+'''
+OpenWeatherMap: 931e14fb72f4c33e24f2756a4f10b5e9
+api.openweathermap.org/data/2.5/weather/city?6232280&APPID=931e14fb72f4c33e24f2756a4f10b5e9
+http://api.openweathermap.org/data/2.5/forecast/city?id=524901&APPID=1111111111
+'''
