daf1e5b79b228c174b42b99c3b35ad2f3964c935
Gustavo Olivares <guolivar@gmail.com>
2017-03-22 22:39:42 +1300

    Move again to PM2.5 for scariness
    Signed-off-by: Gustavo Olivares <guolivar@gmail.com>

--- aq_data/fetch_and_update_AQ.py
+++ aq_data/fetch_and_update_AQ.py
@@ -114,39 +114,24 @@ penr = requests.get(urlPenrose + aqicnkey).json()
 patu = requests.get(urlPotumahoe + aqicnkey).json()
 taka = requests.get(urlTakapuna + aqicnkey).json()
 
-# Get PM10 values
+# Get pm25 values
 try:
-    pm10_glen_value = glen["data"]["iaqi"]['pm10']['v']
+    pm25_penr_value = penr["data"]["iaqi"]['pm25']['v']
 except:
-    pm10_glen_value = None
+    pm25_penr_value = None
 try:
-    pm10_hend_value = hend["data"]["iaqi"]['pm10']['v']
+    pm25_patu_value = patu["data"]["iaqi"]['pm25']['v']
 except:
-    pm10_hend_value = None
+    pm25_patu_value = None
 try:
-    pm10_paku_value = paku["data"]["iaqi"]['pm10']['v']
+    pm25_taka_value = taka["data"]["iaqi"]['pm25']['v']
 except:
-    pm10_paku_value = None
-try:
-    pm10_penr_value = penr["data"]["iaqi"]['pm10']['v']
-except:
-    pm10_penr_value = None
-try:
-    pm10_patu_value = patu["data"]["iaqi"]['pm10']['v']
-except:
-    pm10_patu_value = None
-try:
-    pm10_taka_value = taka["data"]["iaqi"]['pm10']['v']
-except:
-    pm10_taka_value = None
+    pm25_taka_value = None
 
 # Calculate the maximnum value
-pm10 = max(pm10_penr_value,
-             pm10_patu_value,
-             pm10_hend_value,
-             pm10_paku_value,
-             pm10_taka_value,
-             pm10_penr_value,
+pm25 = max(pm25_penr_value,
+             pm25_patu_value,
+             pm25_taka_value,
              1)
 
 # Get NO2 data
@@ -182,20 +167,20 @@ no2 = max(no2_penr_value,
 # The data is only updated hourly so to fill up the time
 # add a loop to update the agent with some noise over the data
 print("Date ", glen["data"]['time']['s'])
-print("PM10 ", pm10)
+print("pm25 ", pm25)
 print("NO2 ", no2)
 # Log in to Imersia's sytem
 sessionid = ImersiaLogin()
 
 # Push data within the minute
 for i in (1, 2, 3, 4, 5):
-    # Extract pm10 and no2 from a wide normal distribution
-    noisy_pm10 = random.lognormvariate(math.log(pm10), 0.1)
+    # Extract pm25 and no2 from a wide normal distribution
+    noisy_pm25 = random.lognormvariate(math.log(pm25), 0.1)
     noisy_no2 = random.lognormvariate(math.log(no2), 0.1)
-    level_pm10 = min(round(10 * (max(0, noisy_pm10 - 5) / 50)), 10)
+    level_pm25 = min(round(10 * (max(0, noisy_pm25 - 5) / 50)), 10)
     level_no2 = min(round(5 * (max(0, noisy_no2 - 0) / 20)), 5)
     print("NO2 level ", level_no2)
-    print("PM10 level ", level_pm10)
+    print("PM2.5 level ", level_pm25)
     # Update the pollution
     # Post a value to set the pollution level (0 to 5) and trigger the devices
     # watching
@@ -205,10 +190,10 @@ for i in (1, 2, 3, 4, 5):
     # Post a value to set the particlespeed level (0 to 10) and trigger the
     # devices watching
     result = SendCommand('agent/metadata/setvalue',
-                         {'agentid': treeID, 'key': 'particlespeed', 'value': level_pm10})
+                         {'agentid': treeID, 'key': 'particlespeed', 'value': level_pm25})
     time.sleep(8)
 print("--- %s seconds ---" % (time.time() - start_time))
 # Update thingspeak channel
-options = {'api_key': writekey, 'field1': pm10, 'field2': no2}
+options = {'api_key': writekey, 'field1': pm25, 'field2': no2}
 req = requests.post(thingspk, data=options)
 print(req)
